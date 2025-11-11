5122ddcd228ee6ef2e8c61dd30cb60c4bdd94bd0
Bartłomiej Gaweł <bartlomiej_gawel@outlook.com>
2025-11-11 11:08:32 +0100

    update npm projects

--- aspire/Demo.AppHost/Program.cs
+++ aspire/Demo.AppHost/Program.cs
@@ -1,20 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("demo-postgres")
-    .WithImage("postgres:latest")
-    .WithBindMount("../../.containers/postgres", "/var/lib/postgresql/18/docker")
-    .WithLifetime(ContainerLifetime.Persistent)
-    .AddDatabase("demo-db");
+// var postgres = builder.AddPostgres("demo-postgres")
+//     .WithImage("postgres:latest")
+//     .WithBindMount("../../.containers/postgres", "/var/lib/postgresql/18/docker")
+//     .WithLifetime(ContainerLifetime.Persistent)
+//     .AddDatabase("demo-db");
 
-builder.AddNpmApp("demo-app", "../../apps/demo-app")
-    .WithReference(weatherApi)
+builder.AddNpmApp("demo-app", "../../apps/demo-app", "dev")
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
-builder.AddNpmApp("demo-backoffice-app", "../../apps/demo-backoffice-app")
-    .WithReference(weatherApi)
+builder.AddNpmApp("demo-backoffice-app", "../../apps/demo-backoffice-app", "dev")
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints()
