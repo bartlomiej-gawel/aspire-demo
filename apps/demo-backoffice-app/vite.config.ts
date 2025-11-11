9234657198863c0725e20d3f8b2dc873a389f29a
Bartłomiej Gaweł <bartlomiej_gawel@outlook.com>
2025-11-11 23:19:29 +0100

    add shadcn to demo-backoffice-app

--- apps/demo-backoffice-app/vite.config.ts
+++ apps/demo-backoffice-app/vite.config.ts
@@ -1,17 +1,24 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
       port: parseInt(env.VITE_PORT),
       proxy: {
         '/api': {
           target:
-            process.env.services__weatherapi__https__0 ||
-            process.env.services__weatherapi__http__0,
+              process.env.services__weatherapi__https__0 ||
+              process.env.services__weatherapi__http__0,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
           secure: false,
