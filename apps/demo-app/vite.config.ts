ba28d278deaa9e83624ad5f74540fd55d444cc42
Bartłomiej Gaweł <bartlomiej_gawel@outlook.com>
2025-11-11 23:12:55 +0100

    add shadcn to demo-app

--- apps/demo-app/vite.config.ts
+++ apps/demo-app/vite.config.ts
@@ -1,10 +1,17 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
       port: parseInt(env.VITE_PORT),
       proxy: {
